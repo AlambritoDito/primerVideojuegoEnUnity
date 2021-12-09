1d0669a0c8b9782537f32a4d528a4a42fafd8c33
Brito <britoditodeveloper@gmail.com>
2021-12-08 23:08:14 -0600

    Primer Commit, se agregaron todos los archivos y el poco progreso que se llevaba

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Variables del movimiento del personaje
+    public float jumForce = 6f;//La f significa newtons
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Void to jump
+    void Jump()
+    {
+
+    }
+}
